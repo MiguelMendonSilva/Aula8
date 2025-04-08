56db5a4eeb92fc32e8400383ccfcbec83c725435
Miguel <124186796+MiguelMendonSilva@users.noreply.github.com>
2025-04-08 20:11:15 -0300

    Update exerc8.c

--- Aula_8/exerc8.c
+++ Aula_8/exerc8.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+int testa_par(int x);
 
+
+int main() {
+    int valor;
+    printf("Digite um valor: ");
+    scanf("%d", &valor);
+
+    return 0;
+}
+
+int testa_par(int x) {
+    if (x % 2 == 0) {
+        return 1;
+    } else{
+        
+        return 0;
+    }
+
+}
