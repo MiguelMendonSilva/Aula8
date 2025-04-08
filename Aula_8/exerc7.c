842410717fcdaf32f6a797288f4ba591b4ef4876
Miguel <124186796+MiguelMendonSilva@users.noreply.github.com>
2025-04-08 20:02:31 -0300

    Update exerc7.c

--- Aula_8/exerc7.c
+++ Aula_8/exerc7.c
@@ -1,23 +1,57 @@
 #include <stdio.h>
 
-int main(void) {
-    
-  int numero;
+int main() {
 
-  printf("digite 4 números ínteiros: ");
-  
-  
-  for(int linha = 0; linha < 4; linha++ ){
-      scanf("%d", &numero);
-        for(int i = 0; i < numero; i++){
-            printf("*");
-        }
-        
-        printf(" ");
-  }
-  
-  
-    
+    int num1 = 0;
+    int num2 = 0;
+    int num3 = 0;
+    int num4 = 0;
 
-  return 0;
+
+    do{
+        printf("Digite o primeiro número: ");
+        scanf("%d", &num1);
+    } while(num1 <0 || num1 > 10);
+    
+    do{
+        printf("Digite o segundo número: ");
+        scanf("%d", &num2);
+    } while(num2 <0 || num2 > 10);
+
+    do{
+        printf("Digite o terceiro número: ");
+        scanf("%d", &num3);
+    } while(num3 <0 || num3 > 10);
+
+    do{
+        printf("Digite o quarto número: ");
+        scanf("%d", &num4);
+    } while(num4 <0 || num4 > 10);
+
+
+    for(int contador = 1; contador <=num1; contador++){
+        printf("*");
+    }
+    printf("\n");
+
+    for(int contador = 1; contador <=num2; contador++){
+        printf("*");
+    }
+    printf("\n");
+
+    for(int contador = 1; contador <=num3; contador++){
+        printf("*");
+    }
+    printf("\n");
+
+    for(int contador = 1; contador <=num4; contador++){
+        printf("*");
+    }
+    printf("\n");
+
+
+
+
+
+    return 0;
 }
